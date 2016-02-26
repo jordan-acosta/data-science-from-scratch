3507c9e7b01d7bf093fd0b78c514dfc0c0d4793b
Jordan Acosta <jordan@solfo.com>
2016-02-26 08:49:24 -0800

    Data Scientists You May Know

--- introduction.py
+++ introduction.py
@@ -1,6 +1,6 @@
+# Finding Key Connectors #
 from __future__ import division
 
-# Finding Key Connectors
 users = [
     { "id": 0, "name": "Hero" },
     { "id": 1, "name": "Dunn" },
@@ -51,11 +51,79 @@ sorted_by_num_friends = sorted(num_friends_by_id, key=lambda (user_id, num_frien
 
 # We've computed "degree centrality".
 # In other words, "identify people who are somehow central to the network".
-print(total_connections)
-print(num_users)
-print(avg_connections)
-print(num_friends_by_id)
-print(sorted_by_num_friends)
 
-# Data Scientists You May Know
+# print(total_connections)
+# print(num_users)
+# print(avg_connections)
+# print(num_friends_by_id)
+# print(sorted_by_num_friends)
 
+# Data Scientists You May Know #
+from collections import Counter
+
+def friends_of_friend_ids_bad(user):
+    # "foaf" is short for "friends of a friend"
+    return [foaf["id"]
+            for friend in user["friends"]
+            for foaf in friend["friends"]]
+
+def not_the_same(user, other_user):
+    return user["id"] != other_user["id"]
+
+def not_friends(user, other_user):
+    return all(not_the_same(friend, other_user)
+            for friend in user["friends"])
+
+def friends_of_friend_ids(user):
+    return Counter(foaf["id"]
+            for friend in user["friends"]
+            for foaf in friend["friends"]
+            if not_the_same(user, foaf)
+            and not_friends(user, foaf))
+
+# print friends_of_friend_ids(users[3])
+
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+def data_scientists_who_like(target_interest):
+    return [user_id
+            for user_id, user_interest in interests
+            if user_interest == target_interest]
+
+# print data_scientists_who_like("Hadoop")
+
+from collections import defaultdict
+
+# is this just a map?
+user_ids_by_interest = defaultdict(list)
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+
+interests_by_user_id = defaultdict(list)
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+
+def most_common_interests_with(user):
+    return Counter(interested_user_id
+            for interest in interests_by_user_id[user["id"]]
+            for interested_user_id in user_ids_by_interest[interest]
+            if interested_user_id != user["id"])
+
+# print most_common_interests_with(users[0])
+
+# Salaries and Experience #
